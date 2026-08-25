07cbece9ac821388499ccbcb6b7536374e9e70f0
Tayler Caufield <taylercaufield@gmail.com>
2026-08-24 19:14:11 -0700

    Fixed routes and added working nav bar

--- PersonalPortfolio/src/App.jsx
+++ PersonalPortfolio/src/App.jsx
@@ -3,18 +3,43 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Contact from './pages/Contact.jsx'
-import './index.css'
+import Projects from './pages/Projects.jsx'
+import Navbar from './components/Navbar.jsx'
 
 
 function App() {
-  
-    return(
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-        </Routes>
-    )
+
+
+  const items = [
+  { label: "Home", href: "#" },
+  { label: "About", href: "#" },
+  { label: "Contact", href: "#" },
+  { label: "Projects", href: "#" }
+  ];
+
+ return (
+    <>
+      <div style={{ height: '600px', position: 'relative' }}>
+        <Navbar
+          items={items}
+          particleCount={15}
+          particleDistances={[90, 10]}
+          particleR={100}
+          initialActiveIndex={0}
+          animationTime={600}
+          timeVariance={300}
+          colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+        />
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/Projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
