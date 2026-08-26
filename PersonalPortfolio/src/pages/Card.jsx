e4a489b51408999d1b70193a57b159565093e461
Tayler Caufield <taylercaufield@gmail.com>
2026-08-26 15:00:44 -0700

    Added card component and information

---
+++ PersonalPortfolio/src/pages/Card.jsx
@@ -0,0 +1,24 @@
+function Card({ title, children }) { 
+    return ( 
+        <div 
+        style={{ 
+            border: "1px solid #bcbcc2", 
+            borderRadius: "12px", 
+            padding: "24px", 
+            boxShadow: "0 2px 8px rgba(0,0,0,0.08)", 
+            backgroundColor: "#656669", 
+            width: "300px",
+            boxSizing: "border-box", 
+        }}
+        > 
+
+        {title && ( 
+            <h2 style={{ fontSize: "24px", marginBottom: "12px", color: "Black", textAlign: "center"}}> {title} </h2>
+        )}
+        {children}
+        
+        </div>
+    )
+}
+
+export default Card
--- PersonalPortfolio/src/pages/home.jsx
+++ PersonalPortfolio/src/pages/home.jsx
@@ -1,18 +1,32 @@
-
+import Card from "./Card";
 function Home() {
   return (
     <>
   <div className = "HomeHeader" style ={{ fontSize: "40px",  }}> Hey, Im Tayler</div>
   <div style={{ fontSize: "20px", marginBottom: "60px", }}> And this is my portfolio. </div>
   
-  <div 
-    style={{ fontSize: "40px", textAlign: "left", flexDirection: "column", gap: "10px", }}
-  >
-    ABOUT ME 
-    <div style={{ fontSize: "18px",}}> Im currently in the last quarter of my Bachelors degree "\n" for software development.</div>
-  </div>
-
- 
+  <div style={{ display: "flex", flexDirection: "row", gap: "75px", flexWrap: "wrap" }}>
+  <Card title="ABOUT ME"> 
+     Im currently in my last quarter of my bachelors degree for Software Development. 
+     I enjoy building full stack web and mobile applications. Im working as a data center technician at AWS where i get to work
+     hands on with networking, troubleshooting, scripts, and computer hardware. 
+  </Card>
+  <Card title="TECH STACK">
+    <ul>
+   <li> Frontend: </li>
+   <li> Backend: </li>
+   <li> Tools: </li>
+    </ul> 
+  </Card>
+  <Card title="HOBBIES">
+    <ul> 
+      <li> Rock Climbing </li>
+      <li> Gaming </li>
+      <li> Hiking </li>
+      <li> Bowling </li>
+    </ul>
+  </Card>
+ </div> 
    </>
   )
 }
