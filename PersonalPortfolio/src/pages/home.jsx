e977fd82b26de289f78140d6979fed2c93685cf4
Tayler Caufield <taylercaufield@gmail.com>
2026-08-26 14:31:59 -0700

    added card component

--- PersonalPortfolio/src/pages/home.jsx
+++ PersonalPortfolio/src/pages/home.jsx
@@ -1,5 +1,20 @@
+
 function Home() {
-  return <div>Home Page</div>
+  return (
+    <>
+  <div className = "HomeHeader" style ={{ fontSize: "40px",  }}> Hey, Im Tayler</div>
+  <div style={{ fontSize: "20px", marginBottom: "60px", }}> And this is my portfolio. </div>
+  
+  <div 
+    style={{ fontSize: "40px", textAlign: "left", flexDirection: "column", gap: "10px", }}
+  >
+    ABOUT ME 
+    <div style={{ fontSize: "18px",}}> Im currently in the last quarter of my Bachelors degree "\n" for software development.</div>
+  </div>
+
+ 
+   </>
+  )
 }
 
 export default Home
