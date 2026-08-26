f8af6403a6867cb1cb46a34c42bdbf54e912bab5
Tayler Caufield <taylercaufield@gmail.com>
2026-08-26 12:13:09 -0700

    updated nav bar and fixed loading bug

--- PersonalPortfolio/src/App.jsx
+++ PersonalPortfolio/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'  
-import RetroBackground, { RetroNavBar } from './components/RetroBackground.jsx'
+import RetroBackground from './components/RetroBackground.jsx'
 import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
+import Resume from './pages/Resume.jsx'
 import Contact from './pages/Contact.jsx'
 import Projects from './pages/Projects.jsx'
-// import Navbar from './components/Navbar.jsx'
+
 
 
 
@@ -14,11 +14,10 @@ function App() {
 
  return (
     <RetroBackground theme="dimblue">
-      <RetroNavBar />
       <main style={{ padding: "24px 32px" }}>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
+        <Route path='/Resume' element={<Resume />} />
         <Route path='/Projects' element={<Projects />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
