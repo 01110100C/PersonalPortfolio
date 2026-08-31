d194ef999e7579b0270c0366a5877de61c499c12
Tayler Caufield <taylercaufield@gmail.com>
2026-08-30 21:07:38 -0700

    added contacts

--- PersonalPortfolio/src/pages/Contact.jsx
+++ PersonalPortfolio/src/pages/Contact.jsx
@@ -1,9 +1,23 @@
+import Card from "./Card"; 
+import { MdEmail } from "reat-icons/md"; 
+import { FaPhoneAlt } from "react-icons/fa"; 
+import { FaLinkedin } from "react-icons/fa"; 
+import { FaGithub } from "react-icons/fa"; 
 function Contact() {
   return (
     <>
   <h1> Lets Connect! </h1>
   <h2> Im currently open to new opportunities. </h2>
-  <div> </div>
+  <div> 
+    <Card> 
+      <ul> 
+        <li> <MdEmail /> Email: taylercaufield@gmail.com</li>
+        <li> <FaPhoneAlt /> Phone: (509)366-2478 </li>
+        <li> <FaLinkedin /> LinkedIn: https://www.linkedin.com/in/taylercaufield/ </li>
+        <li> <FaGithub /> GitHub: https://github.com/01110100C </li>
+      </ul>
+    </Card>
+  </div>
 
     </>
   )
