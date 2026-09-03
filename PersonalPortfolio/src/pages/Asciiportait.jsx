97843696ce5b6f27af5d7d698246c23133d4efb8
Tayler Caufield <taylercaufield@gmail.com>
2026-09-03 14:37:52 -0700

    added ascii picture to home page

---
+++ PersonalPortfolio/src/pages/Asciiportait.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+
+export default function AsciiPortrait({
+  art,
+  speed = 10,
+  color = "white",
+  fontSize = 3,
+}) {
+  const lines = art.replace(/^\n/, "").split("\n");
+  const [visibleCount, setVisibleCount] = useState(0);
+
+  useEffect(() => {
+    setVisibleCount(0);
+    let i = 0;
+    const id = setInterval(() => {
+      i += 1;
+      setVisibleCount(i);
+      if (i >= lines.length) clearInterval(id);
+    }, speed);
+    return () => clearInterval(id);
+   
+  }, [art]);
+
+  return (
+    <pre
+      style={{
+        fontFamily: '"IBM Plex Mono", ui-monospace, monospace',
+        fontSize,
+        lineHeight: 1.05,
+        color,
+        textAlign: "center",
+        margin: "0 auto 20px",
+        display: "inline-block",
+        whiteSpace: "pre",
+      }}
+    >
+      {lines.slice(0, visibleCount).join("\n")}
+    </pre>
+  );
+}
+
+export const PLACEHOLDER_ART = `
+        .-"""""-.
+       /         \\
+      |  o     o  |
+      |     ^     |
+       \\   ---   /
+        '-.....-'
+       /         \\
+      /   PHOTO   \\
+     /   PENDING   \\
+    '---------------'
+`;
--- PersonalPortfolio/src/pages/home.jsx
+++ PersonalPortfolio/src/pages/home.jsx
@@ -1,11 +1,29 @@
 import Card from "./Card";
 import Skillsgrid from "./Skillsgrid"; 
+import AsciiPortait from "./Asciiportait";
+import {  MY_ASCII_ART  } from "./MyAsciiArt";
+
 function Home() {
   return (
     <>
-  <div className = "HomeHeader" style ={{ fontSize: "50px", textAlign: "center", fontFamily: "IBM Plex Sans, monospace" }}> Tayler Caufield</div>
-  <div style={{ fontSize: "20px", marginBottom: "60px", textAlign: "center", fontFamily: "IBM Plex Sans, sans-serif" }}> Software Developer </div>
-  
+   <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "40px",
+          flexWrap: "wrap",
+          marginBottom: "40px",
+        }}
+      >
+        <div>
+          <div className="HomeHeader" style={{ fontSize: "50px", textAlign: "center", fontFamily: "IBM Plex Sans, monospace" }}> Tayler Caufield</div>
+          <div style={{ fontSize: "20px", textAlign: "center", fontFamily: "IBM Plex Sans, sans-serif" }}> Software Developer </div>
+        </div>
+
+        <AsciiPortait art={MY_ASCII_ART} speed={25} />
+      </div>
   <div style={{ display: "flex", flexDirection: "row", gap: "75px", flexWrap: "wrap" }}>
   <Card title="ABOUT ME"> 
       
@@ -28,6 +46,7 @@ function Home() {
   <Card title="EDUCATION AND CERTIFICATIONS">
     <ul style={{ color: "black" }}> 
       <li> Bachelors of Science in Software Development </li>
+      <li> AWS Certified Cloud Practitioner </li>
     </ul> 
   </Card> 
  </div> 
