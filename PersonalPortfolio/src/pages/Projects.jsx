f75261d63fc2cfe0efcecc4cbf855ee3cb18cfa1
Tayler Caufield <taylercaufield@gmail.com>
2026-09-17 14:58:50 -0700

    Adding to projects page

--- PersonalPortfolio/src/pages/Projects.jsx
+++ PersonalPortfolio/src/pages/Projects.jsx
@@ -9,10 +9,11 @@ function Projects() {
 
 <div style={{ display: "flex", flexDirection: "row", gap: "75px", flexWrap: "wrap" }}>
   <Card title="Send It">
-    <p> Send it is a full stack web application that allows rock climbers
+    <p style={{ color: "black" }}> Send it is a full stack web application that allows rock climbers
       to log their daily climbs, connect with friends, find local gyms, compete with others, 
       and get personalized training plans. This is an all-in-one application for rock climbers 
       to use to easily have all the tools they need at the palm of their hand. </p>
+
      <h2> Tech Stack </h2> 
       <ul style={{ color: "black" }}>
         <li> React </li>
@@ -23,6 +24,10 @@ function Projects() {
   </Card>
 
   <Card title="Card Counter"> 
+    <p style={{ color: "black" }}> This project includes a raspberry pi, pi camera, openCV, and python. 
+      I created a website that live streams the camera and when cards are dealt in front of the camera, it can reconize the suit and 
+      value of the card. The software keeps track of which cards have been shown and keep a running count. You can enter how many decks are in the 
+      hand so it can more accurately keep track of the cards being shown. I used python and inputted images of the suits and values.  </p>
 
   </Card>
 
